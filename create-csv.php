31e711b6616f7acaa6d52148458d51eda3757391
Thomas Misson <11tmisson@gmail.com>
2019-12-11 21:13:18 +0000

    Done CSV and started dynamic graphing, need to add dashboard to questions answered graph

--- create-csv.php
+++ create-csv.php
@@ -1,33 +1,47 @@
 <?php
 
-if(isset($_GET['id']) && isset($_GET['ursid']))
+if(isset($_GET['id']))
 {
-    header('Content-Type: text/csv');
-    header('Content-Disposition: attachment; filename="data.csv";');
-
-    $resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_GET[surveyID]");
-    $resultData = mysqli_query($con, "SELECT answer FROM answers INNER JOIN questions ON answers.question = questions.id WHERE questions.survey = $_GET[surveyID]");
-
+    $fh = fopen("php://output", "w");
     $data = array();
     $headers = array();
-    for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
+    require_once 'partials/dbconnection.php';
+    if($resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_GET[id]"))
     {
-        $row = mysqli_fetch_assoc($resultTitles);
-        array_push($headers, $row['title']);
+        for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
+        {
+            $row = mysqli_fetch_assoc($resultTitles);
+            array_push($headers, $row['title']);
+        }
+        fputcsv($fh, $headers);
     }
-    for($i=0; $i<mysqli_num_rows($resultData); $i++)
+    
+    if($resultData = mysqli_query($con, "SELECT answer FROM answers INNER JOIN questions ON answers.question = questions.id WHERE questions.survey = $_GET[id]"))
     {
-        $row = mysqli_fetch_assoc($resultData);
-        array_push($data, $row['answer']);
+        for($i=0; $i<mysqli_num_rows($resultData); $i++)
+        {
+            $row = mysqli_fetch_assoc($resultData);
+            if($i%mysqli_num_rows($resultTitles)==0 && $i>0)
+            {
+                fputcsv($fh, $data);
+                $data = array();
+                array_push($data, $row['answer']);
+            }
+            else{
+                array_push($data, $row['answer']);
+            }
+        }
+        fputcsv($fh, $data);
     }
-
-    $fh = fopen("php://output", "w");
-
-    fputcsv($fh, $headers);
-    fputcsv($fh, $data);
 
     fclose($fh);
 
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="data.csv";');
+
+}
+else{
+    header(403);
 }
 
 ?>
--- view-responses.php
+++ view-responses.php
@@ -1,64 +1,130 @@
 <?php
-    require_once 'partials/builder-viewer-header.php';
-    if(isset($_SESSION['surveyID']))
+require_once 'partials/builder-viewer-header.php';
+if(isset($_SESSION['surveyID']))
+{
+    if(isset($_SESSION['owner']))
     {
-        if(isset($_SESSION['owner']))
+        if($_SESSION['owner'])
         {
-            if($_SESSION['owner'])
-            {
-                $numofResponses = mysqli_query($con, "SELECT count(answer) AS numberOfResponses FROM `answers` INNER JOIN questions on answers.question=questions.id INNER JOIN surveys on questions.survey=surveys.id WHERE surveys.id=$_SESSION[surveyID]");
-                $answersrow = mysqli_fetch_assoc($numofResponses);
-                if($answersrow['numberOfResponses']>0)
-                {                
-                    echo "
-                    <section>
-                        <h2>Responses</h2>
-                            <table>
-                            <tr>";
+            $numofResponses = mysqli_query($con, "SELECT count(answer) AS numberOfResponses FROM `answers` INNER JOIN questions on answers.question=questions.id INNER JOIN surveys on questions.survey=surveys.id WHERE surveys.id=$_SESSION[surveyID]");
+            $answersrow = mysqli_fetch_assoc($numofResponses);
 
-                            $resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
+            if($answersrow['numberOfResponses']>0)
+            {                
+                echo "
+                <section>
+                    <h2>Responses</h2>
+                        <table>
+                        <tr>";
 
-                            for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
+                        $resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
+
+                        for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
+                        {
+                            $row = mysqli_fetch_assoc($resultTitles);
+                            echo"<th>$row[title]</th>";
+                        }
+                        echo "</tr><tr>";
+
+                        $resultData = mysqli_query($con, "SELECT answer FROM answers INNER JOIN questions ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
+
+                        for($i=0; $i<mysqli_num_rows($resultData); $i++)
+                        {
+                            $row = mysqli_fetch_assoc($resultData);
+                            
+                            if($i % (mysqli_num_rows($resultTitles))==0 && $i>0)
                             {
-                                $row = mysqli_fetch_assoc($resultTitles);
-                                echo"<th>$row[title]</th>";
+                                echo "</tr><tr>";
+                                echo"<td>$row[answer]</td>";
                             }
-                            echo "</tr><tr>";
-
-                            $resultData = mysqli_query($con, "SELECT answer FROM answers INNER JOIN questions ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
-
-                            for($i=0; $i<mysqli_num_rows($resultData); $i++)
-                            {
-                                $row = mysqli_fetch_assoc($resultData);
-                                
-                                if($i % (mysqli_num_rows($resultTitles))==0 && $i>0)
-                                {
-                                    echo "</tr><tr>";
-                                    echo"<td>$row[answer]</td>";
-                                }
-                                else{
-                                    echo"<td>$row[answer]</td>";
-                                }
+                            else{
+                                echo"<td>$row[answer]</td>";
                             }
-                            echo "</tr>";
+                        }
+                        echo "</tr>";
 
-                            $noOfRespondants = ($answersrow['numberOfResponses']/mysqli_num_rows($resultTitles));
+                        $noOfRespondants = ($answersrow['numberOfResponses']/mysqli_num_rows($resultTitles));
 
+                    echo"
+                        </table>
+                        <p>No. of respondants: $noOfRespondants</p>
+                        <a href=create-csv.php?id=$_SESSION[surveyID]>Export CSV</a>
+                </section>";
+
+                echo<<<_END
+                <section>
+                    <h2>Graphs</h2>
+                    <script type="text/javascript">
+                        // Load the Visualization API and the corechart package.
+                        google.charts.load('current', {'packages':['corechart']});
+                
+                        // Set a callback to run when the Google Visualization API is loaded.
+                        google.charts.setOnLoadCallback(drawChart);
+                
+                        // Callback that creates and populates a data table,
+                        // instantiates the pie chart, passes in the data and
+                        // draws it.
+                        function drawChart() {
+                
+                        // Create the data table.
+                        var data = new google.visualization.DataTable();
+                        data.addColumn('string', 'Questions');
+                        data.addColumn('number', 'No. of people responded');
+                        data.addRows([
+_END;
+                        $result = mysqli_query($con, "SELECT title, COUNT(answer) as numberResponses FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID] and answers.answer != ' ' GROUP BY title");
+                        
+                        for($k=0; $k<mysqli_num_rows($result); $k++)
+                        {
+                            $row = mysqli_fetch_assoc($result);
+                            $str = "['$row[title]', ". $row['numberResponses']. '],';
+                            echo $str;
+                        }
+                        
+                        $result = mysqli_query($con, "SELECT surveyName FROM surveys WHERE id='$_SESSION[surveyID]'");
+                        $row = mysqli_fetch_assoc($result);
+                        $title = $row['surveyName'];
                         echo"
-                            </table>
-                            <p>No. of respondants: $noOfRespondants</p>
-                            <form method='post'>
-                                <input type='Submit' name='CSV' value='Export CSV'>
-                            </form>
-                    </section>";
+                        ]);
+                
+                        // Set chart options
+                        var options = {'title':'$title response breakdown',
+                                        'width':400,
+                                        'height':300,
+                                    };
+                
+                        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                        chart.draw(data, options);
+                        }
+                        
+                    </script>
 
-                    echo<<<_END
-                    <section>
-                        <h2>Graphs</h2>
-                        <script type="text/javascript">
-                            // Load the Visualization API and the corechart package.
-                            google.charts.load('current', {'packages':['corechart']});
+                    <div id='chart_div'></div>
+
                     
+                    <select id='questionSelector'>";
+                        echo"<option> </option>";
+                    $resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
+                    for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
+                    {
+                        $row = mysqli_fetch_assoc($resultTitles);
+                        echo"<option>$row[title]</option>";
+                    }
+
+            
+
+                echo "</select>
+
+                    <script type='text/javascript'>
+
+                    $(document).ready(function () {
+                        var optionText = $('#questionSelector').val()
+                        $('#questionSelector').on('change',function(){
+                            optionText = $('#questionSelector option:selected').val();
+                            window.location.replace('view-responses.php?question='+optionText);
+
+                            google.charts.load('current', {'packages':['corechart']});
+    
                             // Set a callback to run when the Google Visualization API is loaded.
                             google.charts.setOnLoadCallback(drawChart);
                     
@@ -69,118 +135,62 @@
                     
                             // Create the data table.
                             var data = new google.visualization.DataTable();
-                            data.addColumn('string', 'Questions');
-                            data.addColumn('number', 'No. of people responded');
+                            data.addColumn('string', 'Answer');
+                            data.addColumn('number', 'No. of occurrences');
                             data.addRows([
-_END;
-                            $result = mysqli_query($con, "SELECT title, COUNT(answer) as numberResponses FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID] and answers.answer != ' ' GROUP BY title");
-                            
-                            for($k=0; $k<mysqli_num_rows($result); $k++)
-                            {
-                                $row = mysqli_fetch_assoc($result);
-                                $str = "['$row[title]', ". $row['numberResponses']. '],';
-                                echo $str;
-                            }
-                            
-                            $result = mysqli_query($con, "SELECT surveyName FROM surveys WHERE id='$_SESSION[surveyID]'");
-                            $row = mysqli_fetch_assoc($result);
-                            $title = $row['surveyName'];
-                            echo"
-                            ]);
-                    
-                            // Set chart options
-                            var options = {'title':'".$title." response breakdown',
-                                            'width':400,
-                                            'height':300,
-                                            backgroundColor: '#222',
-                                            textStyle:{color: '#FFF'},
-                                            pieHole:0.2};
-                    
-                            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-                            chart.draw(data, options);
-                            }
-                        </script>
-
-                        <div id='chart_div'></div>
-
                         
-                        <select id='question selector'>";
+                            ";
+                            if(isset($_GET['question']))
+                            {
+                                $result = mysqli_query($con, "SELECT answer, title, count(answer) as countAns FROM answers INNER JOIN questions on questions.id = answers.question INNER JOIN surveys on surveys.id = questions.survey WHERE surveys.id = '$_SESSION[surveyID]' and answers.answer != ' ' and question.title = '$_GET[question]'GROUP BY answer");
+                                for($i =0; $i<mysqli_num_rows($result); $i++)
+                                {
+                                    $row = mysqli_fetch_assoc($result);
+                                    $str = "['$row[answer]', ". $row['countAns']. '],';
+                                    echo $str;
+                                }
+                            }
+                            else{
 
-                        $resultTitles = mysqli_query($con, "SELECT DISTINCT title FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID]");
-                        for($i =0; $i<mysqli_num_rows($resultTitles); $i++)
-                        {
-                            $row = mysqli_fetch_assoc($resultTitles);
-                            echo"<option>$row[title]</option>";
-                        }
-
-                    echo"</select>
-
-                    <script type='text/javascript'>
-                    // Load the Visualization API and the corechart package.
-                    google.charts.load('current', {'packages':['corechart']});
-            
-                    // Set a callback to run when the Google Visualization API is loaded.
-                    google.charts.setOnLoadCallback(drawChart);
-            
-                    // Callback that creates and populates a data table,
-                    // instantiates the pie chart, passes in the data and
-                    // draws it.
-                    function drawChart() {
-            
-                    // Create the data table.
-                    var data = new google.visualization.DataTable();
-                    data.addColumn('string', 'Questions');
-                    data.addColumn('number', 'No. of people responded');
-                    data.addRows([";
-
-                    $result = mysqli_query($con, "SELECT title, COUNT(answer) as numberResponses FROM questions INNER JOIN answers ON answers.question = questions.id WHERE questions.survey = $_SESSION[surveyID] and answers.answer != ' ' GROUP BY title");
+                            }
+                            echo"
+                                ]);
+                        
+                                // Set chart options
+                                var options = {'title':'$row[title]',
+                                                'width':400,
+                                                'height':300,
+                                            };
+                        
+                                var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+                                chart.draw(data, options);
+                            };
+                        })
+                    });
+                
                     
-                    for($k=0; $k<mysqli_num_rows($result); $k++)
-                    {
-                        $row = mysqli_fetch_assoc($result);
-                        $str = "['$row[title]', ". $row['numberResponses']. '],';
-                        echo $str;
-                    }
-                    
-                    $result = mysqli_query($con, "SELECT surveyName FROM surveys WHERE id='$_SESSION[surveyID]'");
-                    $row = mysqli_fetch_assoc($result);
-                    $title = $row['surveyName'];
-                    echo"
-                    ]);
-            
-                    // Set chart options
-                    var options = {'title':'".$title." response breakdown',
-                                    'width':400,
-                                    'height':300,
-                                    backgroundColor: '#222',
-                                    textStyle:{color: '#FFF'},
-                                    pieHole:0.2};
-            
-                    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-                    chart.draw(data, options);
-                    }
-                </script>
-
-                    </section>";
-                }
-                else
-                {
-                    echo "<section><h2>No one has got back to you 😢</h2><br/><p>You may want to consider sharing the survey if it isn't already</p></section>";
-                }
+                    </script>
+                    <div id='chart_div1'></div>
+                </section>";
             }
             else
             {
-                header('Location: view-surveys.php');
+                echo "<section><h2>No one has got back to you 😢</h2><br/><p>You may want to consider sharing the survey if it isn't already</p></section>";
             }
         }
         else
         {
-            header('Location: admin.php');
+            header('Location: view-surveys.php');
         }
     }
     else
     {
-        header('Location: view-surveys.php');
+        header('Location: admin.php');
     }
+}
+else
+{
+    header('Location: view-surveys.php');
+}
 
 ?>
